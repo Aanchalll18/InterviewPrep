febd7e16a1b73d1ce1e19ead64324b7cfaa06253
Aanchallll18 <kumariaachal002@gmail.com>
2025-05-08 22:36:34 +0530

    commit

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,29 @@
 import React from 'react'
 import { BrowserRouter as Router,Routes,Route } from 'react-router-dom';
+import { Toaster } from 'react-hot-toast';
+import LandingPage from './pages/LandingPage';
+import Login from './pages/Auth/Login';
+import SignUp from './pages/Auth/SignUp';
+import Dashboard from './pages/home/Dashboard';
+import Interview from './pages/InterviewPrep/Interview';
+
 
 
 const App = () => {
   return (
     <div>
-      
+      <Router>
+        <Routes>
+          <Route path='/' element={<LandingPage/>}/>
+          <Route path='/login' element={<Login/>}/>
+          <Route path='/signup' element={<SignUp/>}/>
+          <Route path='/dashboard' element={<Dashboard/>}/>
+          <Route path='/interview-prep/:sessionId' element={<Interview/>}/>
+        </Routes>
+      </Router>
+
+
+      <Toaster position="top-center" reverseOrder={false} />  
     </div>
   )
 }
