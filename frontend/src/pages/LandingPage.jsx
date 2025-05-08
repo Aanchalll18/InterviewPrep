ee3d432ce1aabe0ed5b89d42dd4c840943627b3c
Aanchallll18 <kumariaachal002@gmail.com>
2025-05-08 23:40:11 +0530

    commit

--- frontend/src/pages/LandingPage.jsx
+++ frontend/src/pages/LandingPage.jsx
@@ -1,11 +1,58 @@
-import React from 'react'
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 const LandingPage = () => {
-  return (
-    <div>
-      
-    </div>
-  )
-}
+	const navigate = useNavigate();
 
-export default LandingPage
+	const [openAuthModal, setOpenAuthModal] = useState(false);
+	const [currentPage, setCurrentPage] = useState("login");
+
+	const submitHandler = () => {};
+
+	return (
+		<div className="w-full min-h-full bg-[#FFFCEF]">
+			<div className="w-[500px] h-[500px] bg-amber-200/20 blur-[65px] absolute top-0 left-2" />
+			<div className="container mx-auto px-4 pt-6 pb-[200px] relative z-10">
+				<header className="flex justify-between items-center mb-16">
+					<div className="text-xl text-black font-bold">PrepMate</div>
+					<button
+						className="bg-linear-to-r from-[#FF9324] to-[#e99a4b] text-sm font-semibold text-white px-7 py-2.5 rounded-full hover:bg-black hover:text-white border border-white transition-colors cursor-pointer"
+						onClick={() => setOpenAuthModal(true)}
+					>
+						Login
+					</button>
+				</header>
+
+				<div className="flex flex-col md:flex-row items-center">
+					<div className="w-full md:w-1/2 pr-4 mb-8 md:mb-0">
+						<div className="flex items-center justify-left mb-2">
+							<div className="flex items-center gap-2 text-[13px] text-amber-600 font-semibold bg-amber-100 px-3 py-1 rounded-full border border-amber-300">
+                                AI Powered
+                            </div>
+						</div>
+						<h1 className="text-5xl text-black font-medium mb-6 loading-tight">
+							Ace Interview with <br />
+							<span className="text-transparent bg-clip-text bg-[radial-gradient(circle,_#FF9324_0%,_#FCD760_100%)] bg-[length:200%_200%] animate-text-shine font-semibold">
+                                AI-Powered
+                                </span> {" "}
+                                Learning
+						</h1>
+					</div>
+					<div className="w-full md:w-1/2">
+						<p className="text-[17px] text-gray-900 mr-0 md:mr-20 mb-6">
+							Access role-specific interview questions, expand answers on
+							demand, dive deep into key concepts, and organize your prep your
+							way. From preparation to mastery—your all-in-one interview toolkit
+							is here.
+						</p>
+						<button className="" onClick={submitHandler}>
+							Get Started
+						</button>
+					</div>
+				</div>
+			</div>
+		</div>
+	);
+};
+
+export default LandingPage;
--- frontend/src/pages/home/Dashboard.jsx
+++ frontend/src/pages/home/Dashboard.jsx
@@ -2,8 +2,8 @@ import React from 'react'
 
 const Dashboard = () => {
   return (
-    <div>
-      
+    <div className='bg-blue-200'>
+      dashboard
     </div>
   )
 }
